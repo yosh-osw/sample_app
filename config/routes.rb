928a82812e3702bbec628492c7ad9e57738cc007
yosh-osw <yoshi1056@icloud.com>
2018-02-26 11:59:33 +0900

    Finish static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   root 'application#hello'
+=======
+
+  root 'static_pages#home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
+>>>>>>> feb7241... Finish static pages
 end
