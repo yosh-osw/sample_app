5fdaabd6d2c0c21c035e84857a047df766dfd46a
yosh-osw <yoshi1056@icloud.com>
2018-02-27 10:39:11 +0900

    Finish latout and routes

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test 'layout links' do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    get contact_path
+    assert_select 'title', full_title('Contact')
+    get signup_path
+    assert_select 'title', full_title('Signup')
+  end
+
+  test 'full title helper' do
+    assert_equal full_title, 'Ruby on Rails Tutorial Sample App'
+    assert_equal full_title('Help'), 'Help | Ruby on Rails Tutorial Sample App'
+  end
+end
