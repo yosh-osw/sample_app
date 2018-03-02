c7df2fc4796e2f5ee914705d4f732f3c4913096c
yosh-osw <yoshi1056@icloud.com>
2018-03-02 17:43:45 +0900

    Implement basic login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require bootstrap
+//= require jquery
 //= require turbolinks
 //= require_tree .
-//= require jquery
 //= require bootstrap-sprockets
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   resources :users
@@ -9,5 +11,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-
+  get '/login', to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
